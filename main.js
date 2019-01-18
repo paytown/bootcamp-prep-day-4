da70e6336437366bee642f81bff6784ff139f2b5
Payton Lower <paytown@me.com>
2019-01-17 19:45:48 -0800

    input of first and last name variables working properly in console

--- main.js
+++ main.js
@@ -2,10 +2,18 @@
 function greet() {
 
     // get the inputs by
-
+    var firstNameInput = document.getElementById('first-name');
+    var lastNameInput = document.getElementById('last-name');
+    
     // get the values out of input
+    var firstName = firstNameInput.value;
+    var lastName = lastNameInput.value;
+    
+    console.log(firstName, lastName);
 
     // make the string of the greeting
+    var greeting = 'Hello ' + firstName + ' ' + lastName + '!';
+    console.log(greeting);
 
     // display greeting
 
